0b5483996dda3dcfa6635beea4961edbbfb1803d
Ramitha-V <ramithavimala040504@gmail.com>
2025-04-13 07:25:54 +0000

    Fixes #8

--- base/models.py
+++ base/models.py
@@ -36,3 +36,15 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    blog= models.ForeignKey(Blog, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        ordering = ['-created']
+    
+    def __str__(self):
+        return self.body
